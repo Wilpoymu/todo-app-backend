1c99bbf6547eb3005d34022fa4eadcc7f80b6fef
Wilfer Zuluaga <wdzuluaga54@misena.edu.co>
2024-04-04 07:16:35 -0500

    Add User entity with relationships to Project and Task

--- src/main/java/com/adso/todoappbackend/models/User.java
+++ src/main/java/com/adso/todoappbackend/models/User.java
@@ -1,4 +1,74 @@
 package com.adso.todoappbackend.models;
 
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    // User can belong to many projects
+    @ManyToMany
+    @JoinTable(
+            name = "project_user",
+            joinColumns = @JoinColumn(name = "project_id"))
+    private Set<Project> projects = new HashSet<>();
+
+    // User can have many tasks
+    @OneToMany(mappedBy = "user")
+    private Set<Task> tasks = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(Set<Project> projects) {
+        this.projects = projects;
+    }
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Set<Task> tasks) {
+        this.tasks = tasks;
+    }
 }
